445268b1a0e429be7fa350cdac986fb7a1881098
ba138 <basitalyshah51214@gmail.com>
2026-07-03 11:55:36 +0500

    add ui for real word example

--- SwiftThinkingContinousLearning/LongPressBootCamp.swift
+++ SwiftThinkingContinousLearning/LongPressBootCamp.swift
@@ -10,15 +10,37 @@ import SwiftUI
 struct LongPressBootCamp: View {
     @State var isCompleted : Bool = false
     var body: some View {
-        Text(isCompleted ? "Completed" : "Not Completed")
-            .foregroundColor(.white)
-            .padding()
-            .padding(.horizontal,20)
-            .background(isCompleted ? Color.green : Color.gray)
-            .cornerRadius(12)
-            .onLongPressGesture(minimumDuration : 1.0) {
-                isCompleted.toggle()
+//        Text(isCompleted ? "Completed" : "Not Completed")
+//            .foregroundColor(.white)
+//            .padding()
+//            .padding(.horizontal,20)
+//            .background(isCompleted ? Color.green : Color.gray)
+//            .cornerRadius(12)
+//            .onLongPressGesture(minimumDuration : 1.0) {
+//                isCompleted.toggle()
+//            }
+        VStack{
+            Rectangle()
+                .frame(height: 55)
+                .frame(width: 20)
+                .frame(maxWidth: .infinity ,alignment: .leading)
+                .background(.gray)
+                .padding()
+            HStack{
+                Text("Click Here")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.black)
+                    .cornerRadius(12)
+                Text("Reset")
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(.black)
+                    .cornerRadius(12)
+                
             }
+            
+        }
     }
 }
 
