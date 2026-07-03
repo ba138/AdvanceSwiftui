0e04c56b4f77d2437d5663961b8db48d5b8a9793
ba138 <basitalyshah51214@gmail.com>
2026-07-03 11:29:04 +0500

    Initial Commit

---
+++ SwiftThinkingContinousLearning/SwiftThinkingContinousLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftThinkingContinousLearningApp.swift
+//  SwiftThinkingContinousLearning
+//
+//  Created by Basit Ali on 03/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct SwiftThinkingContinousLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
