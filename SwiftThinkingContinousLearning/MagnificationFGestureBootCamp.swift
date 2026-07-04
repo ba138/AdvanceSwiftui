629686ba3cdad2003aa3c7a4571e9969c2cf9a90
ba138 <basitalyshah51214@gmail.com>
2026-07-04 12:45:01 +0500

    add real world example

--- SwiftThinkingContinousLearning/MagnificationFGestureBootCamp.swift
+++ SwiftThinkingContinousLearning/MagnificationFGestureBootCamp.swift
@@ -11,24 +11,60 @@ struct MagnificationFGestureBootCamp: View {
     @State var gestureValve : CGFloat = 0
     @State var lastAmount : CGFloat = 0
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .font(.title)
-            .padding(40)
-            .background(.red)
-            .scaleEffect(1 + gestureValve + lastAmount)
-            .gesture(
-                MagnificationGesture()
-                    .onChanged{ value in
-                        gestureValve = value - 1
-                    }
-                    .onEnded{ value in
-                        lastAmount += gestureValve
-                        gestureValve = 0
-                    }
-            )
+        VStack{
+            HStack{
+                Circle()
+                    .frame(width: 35,height: 35)
+                Text("SwiftFull Thinking")
+                Spacer()
+                Image(systemName: "ellipsis")
+            }
+            .padding(20)
+            Rectangle()
+                .frame(height: 300)
+                .scaleEffect(1 + gestureValve)
+                .gesture(
+                    MagnificationGesture()
+                        .onChanged { value in
+                            // value is MagnificationGesture.Value; use its magnification factor
+                            gestureValve = value - 1
+                        }
+                        .onEnded { _ in
+                            withAnimation(.spring()) {
+                                gestureValve = 0
+                            }
+                        }
+                )
+            HStack{
+                Image(systemName: "heart.fill")
+                Image(systemName: "text.bubble.fill")
+                Spacer()
+            }
+            .padding(.horizontal)
+            .font(.headline)
+            Text("This is caption for my photo")
+                .frame(maxWidth: .infinity,alignment: .leading)
+                .padding(.horizontal)
+        }
+//        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//            .font(.title)
+//            .padding(40)
+//            .background(.red)
+//            .scaleEffect(1 + gestureValve + lastAmount)
+//            .gesture(
+//                MagnificationGesture()
+//                    .onChanged{ value in
+//                        gestureValve = value - 1
+//                    }
+//                    .onEnded{ value in
+//                        lastAmount += gestureValve
+//                        gestureValve = 0
+//                    }
+//            )
     }
 }
 
 #Preview {
     MagnificationFGestureBootCamp()
 }
+
