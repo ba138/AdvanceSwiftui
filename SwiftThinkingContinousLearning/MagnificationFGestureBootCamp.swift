bd3ac6cff23c9c0d95f8a0acc62c9997e35f33c1
ba138 <basitalyshah51214@gmail.com>
2026-07-04 12:26:50 +0500

    add magnification gesture to the boot camp

--- SwiftThinkingContinousLearning/MagnificationFGestureBootCamp.swift
+++ SwiftThinkingContinousLearning/MagnificationFGestureBootCamp.swift
@@ -8,15 +8,17 @@
 import SwiftUI
 
 struct MagnificationFGestureBootCamp: View {
+    @State var gestureValve : CGFloat = 0
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .font(.title)
             .padding(40)
             .background(.red)
+            .scaleEffect(1 + gestureValve)
             .gesture(
                 MagnificationGesture()
                     .onChanged{ value in
-                        
+                        gestureValve = value - 1
                     }
             )
     }
