eb3f3f3b1780aca965c7aff1750ee5df317a5df1
ba138 <basitalyshah51214@gmail.com>
2026-07-04 12:32:16 +0500

    add last amount and end value for gesture magnifcation

--- SwiftThinkingContinousLearning/MagnificationFGestureBootCamp.swift
+++ SwiftThinkingContinousLearning/MagnificationFGestureBootCamp.swift
@@ -9,16 +9,21 @@ import SwiftUI
 
 struct MagnificationFGestureBootCamp: View {
     @State var gestureValve : CGFloat = 0
+    @State var lastAmount : CGFloat = 0
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .font(.title)
             .padding(40)
             .background(.red)
-            .scaleEffect(1 + gestureValve)
+            .scaleEffect(1 + gestureValve + lastAmount)
             .gesture(
                 MagnificationGesture()
                     .onChanged{ value in
                         gestureValve = value - 1
+                    }
+                    .onEnded{ value in
+                        lastAmount += gestureValve
+                        gestureValve = 0
                     }
             )
     }
