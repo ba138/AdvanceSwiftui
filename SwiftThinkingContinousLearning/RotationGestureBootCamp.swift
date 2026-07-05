c6d3e7efc9b827acb51d831889b2b4f8515583e5
ba138 <basitalyshah51214@gmail.com>
2026-07-05 12:12:41 +0500

    add rotation angle

--- SwiftThinkingContinousLearning/RotationGestureBootCamp.swift
+++ SwiftThinkingContinousLearning/RotationGestureBootCamp.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct RotationGestureBootCamp: View {
+    @State var angle : Angle = Angle(degrees: 0)
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
             .foregroundColor(.white)
             .font(.title)
             .padding(40)
             .background(.green)
+            .rotationEffect(angle)
             .cornerRadius(12)
+            .gesture(
+                RotateGesture()
+                    .onChanged{ value in
+                        
+                    }
+            )
     }
 }
 
