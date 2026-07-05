e7015968701c61409088f66c1b16a2b4afbac454
ba138 <basitalyshah51214@gmail.com>
2026-07-05 12:07:00 +0500

    add rotation gesture

---
+++ SwiftThinkingContinousLearning/RotationGestureBootCamp.swift
@@ -0,0 +1,23 @@
+//
+//  RotationGestureBootCamp.swift
+//  SwiftThinkingContinousLearning
+//
+//  Created by Basit Ali on 05/07/2026.
+//
+
+import SwiftUI
+
+struct RotationGestureBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.white)
+            .font(.title)
+            .padding(40)
+            .background(.green)
+            .cornerRadius(12)
+    }
+}
+
+#Preview {
+    RotationGestureBootCamp()
+}
