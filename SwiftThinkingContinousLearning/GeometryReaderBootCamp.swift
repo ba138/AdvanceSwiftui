422430a8ab90ca363e89bc5d7c3171793188612f
ba138 <basitalyshah51214@gmail.com>
2026-07-07 12:10:52 +0500

    add 3d rotation to the rounded rectangle

--- SwiftThinkingContinousLearning/GeometryReaderBootCamp.swift
+++ SwiftThinkingContinousLearning/GeometryReaderBootCamp.swift
@@ -8,19 +8,39 @@
 import SwiftUI
 
 struct GeometryReaderBootCamp: View {
+    func getPercantage(geo : GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentx = geo.frame(in: .global).midX
+        return Double(1-(currentx/maxDistance))
+    }
     var body: some View {
-        GeometryReader{ geometry in
-            HStack(spacing : 0) {
-                
-                Rectangle()
-                    .fill(.red)
-                    .frame(width: geometry.size .width * 0.666 )
-                Rectangle()
-                    .fill(.blue)
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack{
+                ForEach(0..<50) { index in
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 12)
+                            .rotation3DEffect(Angle(degrees: getPercantage(geo: geometry) * 40),
+                                              axis: (x: 0, y: 1.0, z: 0))
+                            
+                    }
+                    .frame(width: 300 ,height: 250)
+                        .padding()
+                    
+                }
             }
-            .ignoresSafeArea()
-
         }
+//        GeometryReader{ geometry in
+//            HStack(spacing : 0) {
+//                
+//                Rectangle()
+//                    .fill(.red)
+//                    .frame(width: geometry.size .width * 0.666 )
+//                Rectangle()
+//                    .fill(.blue)
+//            }
+//            .ignoresSafeArea()
+//
+//        }
     }
 }
 
