2f0cc03e41493a1301576b857db8bbebf90fa14c
ba138 <basitalyshah51214@gmail.com>
2026-07-08 11:38:12 +0500

    add logic for selecting the model

--- SwiftThinkingContinousLearning/MultiSheetBottomCamp.swift
+++ SwiftThinkingContinousLearning/MultiSheetBottomCamp.swift
@@ -6,15 +6,20 @@
 //
 
 import SwiftUI
-
+struct RendomModel : Identifiable {
+    let id = UUID().uuidString
+    let title : String
+}
 struct MultiSheetBottomCamp: View {
+    @State var selectedModel : RendomModel = RendomModel(title: "Starting model")
     var body: some View {
         VStack (spacing : 20){
             Button("Button1") {
-                
+                selectedModel = RendomModel(title: "One")
             }
             Button("Button2") {
-                
+                selectedModel = RendomModel(title: "Two")
+
             }
         }
     }
