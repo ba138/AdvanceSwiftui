6a7a3ab0571c6ccf8d8f38db430806e33071508a
ba138 <basitalyshah51214@gmail.com>
2026-07-11 11:39:33 +0500

    add default vibration to the boot camp

--- SwiftThinkingContinousLearning/VibrationBootCamp.swift
+++ SwiftThinkingContinousLearning/VibrationBootCamp.swift
@@ -6,21 +6,71 @@
 //
 
 import SwiftUI
+import UIKit
+
 class Heptics{
     static let instance = Heptics() // singleton
     func notification(type : UINotificationFeedbackGenerator.FeedbackType)
     {
-      let generator = UINotificationFeedbackGenerator()
+        let generator = UINotificationFeedbackGenerator()
+        generator.prepare()
         generator.notificationOccurred(type)
     }
-    func impact()
-    {
-        
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.prepare()
+        generator.impactOccurred()
+    }
+    func selectionChanged() {
+        let generator = UISelectionFeedbackGenerator()
+        generator.prepare()
+        generator.selectionChanged()
     }
 }
+
 struct VibrationBootCamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            Text("Haptics Demo")
+                .font(.title)
+
+            Button("Success Notification") {
+                Heptics.instance.notification(type: .success)
+            }
+            Button("Warning Notification") {
+                Heptics.instance.notification(type: .warning)
+            }
+            Button("Error Notification") {
+                Heptics.instance.notification(type: .error)
+            }
+
+            Divider()
+
+            Button("Impact Light") {
+                Heptics.instance.impact(style: .light)
+            }
+            Button("Impact Medium") {
+                Heptics.instance.impact(style: .medium)
+            }
+            Button("Impact Heavy") {
+                Heptics.instance.impact(style: .heavy)
+            }
+            if #available(iOS 13.0, *) {
+                Button("Impact Soft") {
+                    Heptics.instance.impact(style: .soft)
+                }
+                Button("Impact Rigid") {
+                    Heptics.instance.impact(style: .rigid)
+                }
+            }
+
+            Divider()
+
+            Button("Selection Changed") {
+                Heptics.instance.selectionChanged()
+            }
+        }
+        .padding()
     }
 }
 
