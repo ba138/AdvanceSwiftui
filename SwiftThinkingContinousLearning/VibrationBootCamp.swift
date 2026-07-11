5f472061bc9e6a33b5dd6f0571d94b815e70f511
ba138 <basitalyshah51214@gmail.com>
2026-07-11 11:27:50 +0500

    add sigleton class to the boot camp

---
+++ SwiftThinkingContinousLearning/VibrationBootCamp.swift
@@ -0,0 +1,20 @@
+//
+//  VibrationBootCamp.swift
+//  SwiftThinkingContinousLearning
+//
+//  Created by Basit Ali on 11/07/2026.
+//
+
+import SwiftUI
+class Heptics{
+    static let instance = Heptics() // singleton 
+}
+struct VibrationBootCamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VibrationBootCamp()
+}
