92b256d3364f1e337f2a3daa6bb0132bd20e05fb
ba138 <basitalyshah51214@gmail.com>
2026-07-11 11:31:35 +0500

    ädd ui notifcation generator to the boot camp

--- SwiftThinkingContinousLearning/VibrationBootCamp.swift
+++ SwiftThinkingContinousLearning/VibrationBootCamp.swift
@@ -7,7 +7,16 @@
 
 import SwiftUI
 class Heptics{
-    static let instance = Heptics() // singleton 
+    static let instance = Heptics() // singleton
+    func notification(type : UINotificationFeedbackGenerator.FeedbackType)
+    {
+      let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    func impact()
+    {
+        
+    }
 }
 struct VibrationBootCamp: View {
     var body: some View {
