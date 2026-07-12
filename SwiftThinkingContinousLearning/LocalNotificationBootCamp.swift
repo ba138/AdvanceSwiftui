595af1b17f456eab7d2f696a68772b61be54c069
ba138 <basitalyshah51214@gmail.com>
2026-07-12 11:58:40 +0500

    add function for notification permissions

--- SwiftThinkingContinousLearning/LocalNotificationBootCamp.swift
+++ SwiftThinkingContinousLearning/LocalNotificationBootCamp.swift
@@ -6,14 +6,27 @@
 //
 
 import SwiftUI
+import UserNotifications
 class NotificationManger{
     static let instance = NotificationManger()
+    func requestauth(){
+        let option : UNAuthorizationOptions = [
+            .alert,.sound,.badge
+        ]
+        UNUserNotificationCenter.current().requestAuthorization(options: option){(success,error) in
+            if let error = error {
+                print("Error : \(error)")
+            }else{
+                print("success : \(success)")
+            }
+        }
+    }
 }
 struct LocalNotificationBootCamp: View {
     var body: some View {
         VStack(spacing: 20) {
             Button("Request Permission") {
-                
+                NotificationManger.instance.requestauth()
             }
         }
     }
--- SwiftThinkingContinousLearning/SwiftThinkingContinousLearningApp.swift
+++ SwiftThinkingContinousLearning/SwiftThinkingContinousLearningApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftThinkingContinousLearningApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LocalNotificationBootCamp()
         }
     }
 }
