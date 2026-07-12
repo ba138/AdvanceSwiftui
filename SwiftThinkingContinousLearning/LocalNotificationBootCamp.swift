be33d6b6c07bd9c26efc4852e68a456652209121
ba138 <basitalyshah51214@gmail.com>
2026-07-12 11:49:16 +0500

    add content for local notification and add skelton class for notufciation

---
+++ SwiftThinkingContinousLearning/LocalNotificationBootCamp.swift
@@ -0,0 +1,24 @@
+//
+//  LocalNotificationBootCamp.swift
+//  SwiftThinkingContinousLearning
+//
+//  Created by Basit Ali on 12/07/2026.
+//
+
+import SwiftUI
+class NotificationManger{
+    static let instance = NotificationManger()
+}
+struct LocalNotificationBootCamp: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Button("Request Permission") {
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    LocalNotificationBootCamp()
+}
