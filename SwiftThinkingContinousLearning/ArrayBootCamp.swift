5a252876c7058a23d1fc31b1a485c9999cd6dd30
ba138 <basitalyshah51214@gmail.com>
2026-07-14 11:26:01 +0500

    add more user to the list

--- SwiftThinkingContinousLearning/ArrayBootCamp.swift
+++ SwiftThinkingContinousLearning/ArrayBootCamp.swift
@@ -22,13 +22,19 @@ class ArrayModificationViewModel: ObservableObject {
     }
 
     func getUsers() {
-        let user1 = UserModel(name: "Basit", points: 5, isVerfied: false)
+        let user1 = UserModel(name: "Basit", points: 5, isVerfied: true)
         let user2 = UserModel(name: "Fayyaz", points: 5, isVerfied: true)
         let user3 = UserModel(name: "Touseef", points: 4, isVerfied: true)
         let user4 = UserModel(name: "Atizaz", points: 10, isVerfied: false)
+        let user5 = UserModel(name: "Ali", points: 5, isVerfied: true)
+        let user6 = UserModel(name: "Hussain", points: 5, isVerfied: true)
+        let user7 = UserModel(name: "Hassan", points: 4, isVerfied: true)
+        let user8 = UserModel(name: "Haider", points: 10, isVerfied: false)
+        let user9 = UserModel(name: "Hassan", points: 4, isVerfied: true)
+        let user10 = UserModel(name: "Haider", points: 10, isVerfied: false)
 
         self.dataArray.append(
-            contentsOf: [user1, user2, user3, user4]
+            contentsOf: [user1, user2, user3, user4 ,user5 ,user6 ,user7 ,user8 ,user9 ,user10]
 )
     }
 }
@@ -38,7 +44,24 @@ struct ArrayBootCamp: View {
         ScrollView {
             VStack(spacing : 20) {
                 ForEach(vm.dataArray) { item in
-                    Text(item.name)
+                    VStack(alignment : .leading){
+                        Text(item.name)
+                            .font(.headline)
+                        HStack{
+                            Text("\(item.points)")
+                            Spacer()
+                            if item.isVerfied{
+                                Image(systemName: "flame.fill")
+                            }  else{
+                                
+                            }
+                        }
+                    }
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.blue.cornerRadius(10))
+                    .padding(.horizontal)
+                    
                 }
             }
         }
