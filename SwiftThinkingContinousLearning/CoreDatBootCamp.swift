e3f882d48335e35faa08585a5bd0aef9cec75da2
ba138 <basitalyshah51214@gmail.com>
2026-07-17 11:17:55 +0500

    add fruits entity table

--- SwiftThinkingContinousLearning/CoreDatBootCamp.swift
+++ SwiftThinkingContinousLearning/CoreDatBootCamp.swift
@@ -12,7 +12,7 @@ class CoreDataViewModel : ObservableObject {
     let container: NSPersistentContainer
     init() {
         // Use your Core Data model name here instead of "SwiftThinkingContinousLearning" if different
-        container = NSPersistentContainer(name: "")
+        container = NSPersistentContainer(name: "FruitsContainer")
         container.loadPersistentStores { description, error in
             if let error = error {
                 print("Error loading Core Data store: \(error)")
