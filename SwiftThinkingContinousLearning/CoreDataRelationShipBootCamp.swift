93144e4794cffb226783f55118ee384520007c70
ba138 <basitalyshah51214@gmail.com>
2026-07-19 11:11:17 +0500

    add business view to the boot camp

--- SwiftThinkingContinousLearning/CoreDataRelationShipBootCamp.swift
+++ SwiftThinkingContinousLearning/CoreDataRelationShipBootCamp.swift
@@ -76,17 +76,49 @@ struct CoreDataRelationShipBootCamp: View {
                         .cornerRadius(12)
                         .padding()
                 }
-                List {
-                    ForEach(vm.business) { business in
-                        Text(business.name ?? "")
+                ScrollView (.horizontal,showsIndicators: true){
+                    HStack(alignment : .top
+                    ) {
+                        ForEach(vm.business) { business in
+                            BusinessView(entity: business)
+                        }
                     }
-                }.listStyle(.plain)
+                }
+               
             }
             .navigationTitle("RelationShips")
 
         }
 
          
+    }
+}
+struct BusinessView : View{
+    let entity : BusinessEntity
+    var body : some View {
+        VStack( alignment : .leading ,spacing: 20) {
+            Text("Name : \(entity.name ?? "")")
+                .bold()
+            if let department = entity.departments? .allObjects  as?[BusinessEntity]{
+                Text("Department")
+                    .bold()
+                ForEach(department) { department in
+                    Text(department.name ?? "")
+                }
+            }
+            if let employee = entity.employees? .allObjects as?[EmployeeEntity]{
+                Text("Employee name")
+                    .bold()
+                ForEach(employee) { employee in
+                    Text(employee.name ?? "")
+                }
+            }
+        }
+        .padding()
+        .frame(maxWidth: 300 ,alignment: .leading)
+        .background(Color.gray.opacity(0.3))
+        .cornerRadius(10)
+        .shadow(radius: 10)
     }
 }
 
