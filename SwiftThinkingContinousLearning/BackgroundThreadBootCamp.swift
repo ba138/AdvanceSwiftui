ccfc0be34da2c28b48c5a9fe1aaea95ce25200d0
ba138 <basitalyshah51214@gmail.com>
2026-07-21 11:19:50 +0500

    add a background thread for downloading data in bootcamp

--- SwiftThinkingContinousLearning/BackgroundThreadBootCamp.swift
+++ SwiftThinkingContinousLearning/BackgroundThreadBootCamp.swift
@@ -11,8 +11,11 @@ class BackgroundThreadViewModel : ObservableObject {
     @Published var dataArray : [String] = []
     func fetchData()
     {
-     let newData = downloadData()
-        dataArray = newData
+        DispatchQueue.global().async {
+            let newData = self.downloadData()
+            self.dataArray = newData
+        }
+    
     }
    private func downloadData()-> [String]{
         var data : [String] = []
