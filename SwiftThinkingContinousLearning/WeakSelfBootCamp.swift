d415f050966f3eb8861b56dfb19e6784a08430f4
ba138 <basitalyshah51214@gmail.com>
2026-07-22 11:25:35 +0500

    init and deinit the userdefault for track of count

--- SwiftThinkingContinousLearning/WeakSelfBootCamp.swift
+++ SwiftThinkingContinousLearning/WeakSelfBootCamp.swift
@@ -8,9 +8,24 @@
 import SwiftUI
 import Combine
 struct WeakSelfBootCamp: View {
+    @AppStorage("count") var count : Int?
     var body: some View {
         NavigationView {
           NavigationLink("Navigate", destination: WeakSelfScreen())
+                .overlay (
+                    VStack{
+                        
+                        Text("\(count ?? 0)")
+                            .font(.largeTitle)
+                           
+                            .padding()
+                            .background(Color.green.cornerRadius(10))
+                        Spacer()
+                    }
+                        .frame(height: 200)
+                        
+                    
+                )
         }
     }
 }
@@ -31,10 +46,14 @@ class WeakSelfBootViewModel : ObservableObject {
     @Published var data :String? = nil
     init(){
      print("getting data")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount+1, forKey: "count")
         fetchData()
     }
     deinit{
         print("deinit data")
+        let currentCount = UserDefaults.standard.integer(forKey: "count")
+        UserDefaults.standard.set(currentCount-1, forKey: "count")
     }
     func fetchData(){
       data = "new data"
