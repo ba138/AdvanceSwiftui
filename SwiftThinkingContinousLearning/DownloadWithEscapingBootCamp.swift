d05a262031eef92bc0affcf2272e5415e8ed8f3a
ba138 <basitalyshah51214@gmail.com>
2026-07-27 12:00:20 +0500

    make code more readable

--- SwiftThinkingContinousLearning/DownloadWithEscapingBootCamp.swift
+++ SwiftThinkingContinousLearning/DownloadWithEscapingBootCamp.swift
@@ -20,41 +20,56 @@ class DownloadViewModel : ObservableObject {
     }
     func getPosts(){
         guard  let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else {return}
-        URLSession.shared.dataTask(with: url) { (data, response, error ) in
-            guard let data = data else{
-                print("no data.")
-                return
-            }
-            guard  error == nil else {
-                print("error : \(String(describing: error))")
-            return
-            }
-            guard let response  = response as? HTTPURLResponse else{
-                print("Invalid Response.")
-                return
-            }
-            guard response.statusCode >= 200 && response.statusCode < 300 else{
-                print("status code is 2xx but is \(response.statusCode)")
-                return
-            }
-            print("Sucessfully download data!")
-            print(data)
-            let jsonStirng = String(data: data, encoding: .utf8)
-            print(jsonStirng)
-            guard let newPost = try? JSONDecoder().decode(PostModel.self, from: data) else {
-                return
-            }
-            DispatchQueue.main.async { [weak self] in
-                self?.post.append(newPost)
+        downloadData(fromUrl: url) { returnedData in
+            if let data = returnedData {
+                guard let newPost = try? JSONDecoder().decode(PostModel.self, from: data) else {
+                    return
+                }
+                DispatchQueue.main.async { [weak self] in
+                    self?.post.append(newPost)
 
+                }
+            } else {
+                print("No Data returned.")
             }
+        }
+    
+    }
+    func downloadData(fromUrl url : URL ,completionHandler : @escaping (_ data : Data?) -> () ){
+       
+        URLSession.shared.dataTask(with: url) { (data, response, error ) in
+            guard
+                let data = data ,
+            error == nil ,
+                let response  = response as? HTTPURLResponse ,
+                response.statusCode >= 200 && response.statusCode < 300
+            else {
+                print("Error getting data")
+                completionHandler(nil)
+
+                return
+                
+            }
+            
+          
+          completionHandler(data)
+         
         }.resume()
     }
 }
 struct DownloadWithEscapingBootCamp: View {
     @StateObject var vm = DownloadViewModel()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(vm.post) { post in
+                VStack{
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .foregroundColor(.gray)
+                }
+            }
+        }
     }
 }
 
