7dc4d2da2b71942bcf74d6b47103cc568151ebc7
ba138 <basitalyshah51214@gmail.com>
2026-07-27 11:20:49 +0500

    handle all the data form the api

--- SwiftThinkingContinousLearning/DownloadWithEscapingBootCamp.swift
+++ SwiftThinkingContinousLearning/DownloadWithEscapingBootCamp.swift
@@ -12,7 +12,29 @@ class DownloadViewModel : ObservableObject {
         
     }
     func getPosts(){
-        
+        guard  let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else {return}
+        URLSession.shared.dataTask(with: url) { (data, response, error ) in
+            guard let data = data else{
+                print("no data.")
+                return
+            }
+            guard  error == nil else {
+                print("error : \(String(describing: error))")
+            return
+            }
+            guard let response  = response as? HTTPURLResponse else{
+                print("Invalid Response.")
+                return
+            }
+            guard response.statusCode >= 200 && response.statusCode < 300 else{
+                print("status code is 2xx but is \(response.statusCode)")
+                return
+            }
+            print("Sucessfully download data!")
+            print(data)
+            let jsonStirng = String(data: data, encoding: .utf8)
+            print(jsonStirng)
+        }.resume()
     }
 }
 struct DownloadWithEscapingBootCamp: View {
